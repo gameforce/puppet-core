8c101be29ac3a0360e55a618ce216e0d0e11e005
Nick Gotsinas <darf@gameforce.ca>
2016-08-29 23:34:08 -0400

    change systems uid to 67

--- manifests/user.pp
+++ manifests/user.pp
@@ -12,7 +12,7 @@ class user {
     password_max_age => '99999',
     password_min_age => '0',
     shell            => '/bin/zsh',
-    uid              => '1000',
+    uid              => '67',
     comment          => 'systems',
     groups           => [ 'users','puppet' ]
   }
