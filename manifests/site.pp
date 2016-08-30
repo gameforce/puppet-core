34561156faa1585f39061acb2a488330c3ff3d9f
Nick Gotsinas <darf@gameforce.ca>
2016-08-29 23:42:47 -0400

    add vmwaretools class

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,10 @@
 hiera_include('classes')
 
 # Include default modules
-node default {
-  include firewall
-  include ntp
-  include shell
-  include user
-  include package
-}
+#node default {
+#  include firewall
+#  include ntp
+#  include shell
+#  include user
+#  include package
+#}
