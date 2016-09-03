89d239707d28f8de72059ce26268944f8976ac13
root <root@kam1.gameforce.net>
2016-09-02 22:43:00 -0400

    add htop

--- site/modules/base/manifests/packages.pp
+++ site/modules/base/manifests/packages.pp
@@ -1,8 +1,9 @@
 class base::packages {
-  package { 'screen': ensure => 'installed', }
   package { 'epel-release': ensure => 'installed', }
   package { 'git': ensure => 'installed', }
-  package { 'vim-enhanced': ensure => 'installed', }
+  package { 'htop': ensure => 'installed', }
   package { 'nmap': ensure => 'installed', }
+  package { 'screen': ensure => 'installed', }
+  package { 'vim-enhanced': ensure => 'installed', }
   package { 'zsh': ensure => 'installed', }
 }
