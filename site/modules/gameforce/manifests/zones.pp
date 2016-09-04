100bb5a561dad99265afbbc7dce86c0354f2ef35
Nick <nick@gameforce.net>
2016-09-03 23:42:24 -0400

    site modules?

--- site/modules/gameforce/manifests/zones.pp
+++ site/modules/gameforce/manifests/zones.pp
@@ -3,11 +3,11 @@ class gameforce::zones {
 include bind
 
 bind::server::file { 'gameforce.net':
-  source => 'puppet:///modules/gameforce/dns/static.gameforce.net',
+  source => 'puppet:///site/modules/gameforce/dns/static.gameforce.net',
   }
 
 bind::server::file { '11.168.192.in-addr.arpa':
-  source => 'puppet:///modules/gameforce/dns/11.168.192.in-addr.arpa',
+  source => 'puppet:///site/modules/gameforce/dns/11.168.192.in-addr.arpa',
   }
 
 #bind::server::file { [ 'static.gameforce.net', 'static.11.168.192.in-addr.arpa' ]:
