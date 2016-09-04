e95d60e9770bdd1d0817cbd8d1c127391ee43297
Nick <nick@gameforce.net>
2016-09-03 22:19:52 -0400

    remove duplicate declarations of zone files

--- site/modules/gameforce/manifests/zones.pp
+++ site/modules/gameforce/manifests/zones.pp
@@ -1,12 +1,12 @@
 class gameforce::zones {
 
-bind::server::file { 'gameforce.net':
-  source => 'puppet:///modules/gameforce/dns/static.gameforce.net',
-  }
+#bind::server::file { 'gameforce.net':
+#  source => 'puppet:///modules/gameforce/dns/static.gameforce.net',
+#  }
 
-bind::server::file { '11.168.192.in-addr.arpa':
-  source => 'puppet:///modules/gameforce/dns/11.168.192.in-addr.arpa',
-  }
+#bind::server::file { '11.168.192.in-addr.arpa':
+#  source => 'puppet:///modules/gameforce/dns/11.168.192.in-addr.arpa',
+#  }
 
 bind::server::file { [ 'gameforce.net', '11.168.192.in-addr.arpa' ]:
   source_base => 'puppet:///modules/gameforce/dns/',
