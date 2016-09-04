eafd30dccd0408d1f81b021430fcbdb5238720e8
Nick <nick@gameforce.net>
2016-09-03 22:15:22 -0400

    create gameforce::zones class

--- site/modules/gameforce/manifests/zones.pp
+++ site/modules/gameforce/manifests/zones.pp
@@ -1,12 +1,15 @@
+class gameforce::zones {
 
-gameforce::bind::server::file { 'gameforce.net':
+bind::server::file { 'gameforce.net':
   source => 'puppet:///modules/gameforce/dns/static.gameforce.net',
-}
+  }
 
-gameforce::bind::server::file { '11.168.192.in-addr.arpa':
+bind::server::file { '11.168.192.in-addr.arpa':
   source => 'puppet:///modules/gameforce/dns/11.168.192.in-addr.arpa',
-}
+  }
 
-gameforce::bind::server::file { [ 'gameforce.net', '11.168.192.in-addr.arpa' ]:
+bind::server::file { [ 'gameforce.net', '11.168.192.in-addr.arpa' ]:
   source_base => 'puppet:///modules/gameforce/dns/',
+  }
+
 }
