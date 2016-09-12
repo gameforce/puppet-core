4ec90542d707487ac678999eec318b89795be71b
Nick Gotsinas <darf@gameforce.ca>
2016-09-12 12:07:32 -0400

    try to modify secure_path

--- site/profile/manifests/sudo.pp
+++ site/profile/manifests/sudo.pp
@@ -4,5 +4,10 @@
   sudo::conf { 'systems':
   content => "%systems ALL=(ALL) NOPASSWD: ALL",
   }
-  
+
+  sudo::conf { 'secure_path':
+  content => "/opt/puppetlabs/bin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" 
+
+  }
+
 }
