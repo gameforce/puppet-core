e3624aae45cc31ca969127599255dc5db4c1d536
Nick Gotsinas <darf@gameforce.ca>
2016-09-12 12:25:19 -0400

    missing comma

--- site/profile/manifests/sudo.pp
+++ site/profile/manifests/sudo.pp
@@ -1,12 +1,8 @@
 # requires saz-sudo from the forge
   class profile::sudo {
 
-  #sudo::conf { 'systems':
-  #content => "%systems ALL=(ALL) NOPASSWD: ALL",
-  #}
-
   sudo::conf { 'systems':
-  priority => 10
+  priority => 10,
   source => 'puppet:///files/etc/sudoers.d/systems.conf',
 
   }
