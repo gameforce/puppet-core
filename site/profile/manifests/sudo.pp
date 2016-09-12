fbc2f67110406011fd5565ddbe5fd894ae4fc4d4
Nick Gotsinas <darf@gameforce.ca>
2016-09-12 12:09:43 -0400

    try to change secure_path try #2

--- site/profile/manifests/sudo.pp
+++ site/profile/manifests/sudo.pp
@@ -6,7 +6,7 @@
   }
 
   sudo::conf { 'secure_path':
-  content => "/opt/puppetlabs/bin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" 
+  content => "secure_path /opt/puppetlabs/bin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" 
 
   }
 
