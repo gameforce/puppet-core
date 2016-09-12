47dcc555de9d558ae7a77258fd375d249e32eedd
Nick Gotsinas <darf@gameforce.ca>
2016-09-12 14:58:30 -0400

    fixing dhcp class

--- site/profile/manifests/dhcp.pp
+++ site/profile/manifests/dhcp.pp
@@ -1,10 +1,12 @@
-#class { 'profile::dhcpd':
+class { profile::dhcpd
 #  configsource => 'puppet:///modules/profile/dhcp/dhcpd.conf',
 #  # Restrict listening to a single interface
 #  dhcpdargs => 'eth0',
 #  # Default is to enable but allow to be stopped (for active/passive)
 #  ensure => 'running',
 #}
+
+include dhcpd
 
 # Variables used from inside the template
 $dhcpd_netmask             = '255.255.255.0'
@@ -15,8 +17,8 @@ $dhcpd_range_start         = '100'
 $dhcpd_range_end           = '254'
 $dhcpd_default_lease_time  = '3600'
 $dhcpd_max_lease_time      = '21600'
-class { 'profile::dhcp':
-    include dhcpd
+class { '::dhcp':
     configcontent => template('dhcpd/dhcpd.conf-simple.erb'),
       ensure      => 'running',
+      }
 }
