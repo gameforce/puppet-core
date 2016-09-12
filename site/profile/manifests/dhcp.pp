95481072ebb5ccb59246f96c8f448c2a8180ae72
Nick Gotsinas <darf@gameforce.ca>
2016-09-12 15:02:14 -0400

    fix brace location

--- site/profile/manifests/dhcp.pp
+++ site/profile/manifests/dhcp.pp
@@ -1,10 +1,10 @@
-class { profile::dhcp
+class profile::dhcp {
+
 #  configsource => 'puppet:///modules/profile/dhcp/dhcpd.conf',
 #  # Restrict listening to a single interface
 #  dhcpdargs => 'eth0',
 #  # Default is to enable but allow to be stopped (for active/passive)
 #  ensure => 'running',
-#}
 
 include dhcpd
 
