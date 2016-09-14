3dece06baba86f9ca04ebdacae4a24c8d973a514
Nick Gotsinas <darf@gameforce.ca>
2016-09-13 21:14:22 -0400

    add named.conf

--- site/profile/manifests/bind.pp
+++ site/profile/manifests/bind.pp
@@ -3,20 +3,25 @@ class profile::bind {
   include bind
 
   bind::server::conf { '/etc/named.conf':
-    listen_on_addr    => [ 'any' ],
-    listen_on_v6_addr => [ 'any' ],
-    forwarders        => [ '8.8.8.8', '8.8.4.4' ],
-    allow_query       => [ 'localnets' ],
-    zones             => {
-      'gameforce.net' => [
-        'type master',
-        'file "gameforce.net"',
-      ],
-      '11.168.192.in-addr.arpa' => [
-        'type master',
-        'file "11.168.192.in-addr.arpa"',
-      ],
-    },
+    #listen_on_addr    => [ 'any' ],
+    #listen_on_v6_addr => [ 'any' ],
+    #forwarders        => [ '8.8.8.8', '8.8.4.4' ],
+    #allow_query       => [ 'localnets' ],
+    #zones             => {
+    #  'gameforce.net' => [
+    #    'type master',
+    #    'file "gameforce.net"',
+    #  ],
+    #  '11.168.192.in-addr.arpa' => [
+    #    'type master',
+    #    'file "11.168.192.in-addr.arpa"',
+    #  ],
+    #},
+
+  source => 'puppet:///puppet-files/bind/named.conf',
+
+}
+
   }
   bind::server::file { 'gameforce.net':
     source => 'puppet:///puppet-files/bind/gameforce.net',
