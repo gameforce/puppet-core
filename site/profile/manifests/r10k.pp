9c7ba007934d675646be780eee67a46abb1a5fed
Nick Gotsinas <darf@gameforce.ca>
2016-09-15 16:07:51 -0400

    comment out the webhook there's a bug

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -5,18 +5,19 @@ class { 'r10k':
   remote => 'git@github.com:gameforce/puppet-core.git',
 }
 
+# Comment the following out because there is a bug with the webhook
 # Instead of running via mco, run r10k directly
-class {'r10k::webhook::config':
-  use_mcollective => false,
-}
+#class {'r10k::webhook::config':
+#  use_mcollective => false,
+#}
 
 # The hook needs to run as root when not running using mcollective
 # It will issue r10k deploy environment <branch_from_gitlab_payload> -p
 # When git pushes happen.
-class {'r10k::webhook':
-  use_mcollective => false,
-  user            => 'root',
-  group           => '0',
-  require         => Class['r10k::webhook::config'],
-  }
-}
+#class {'r10k::webhook':
+#  use_mcollective => false,
+#  user            => 'root',
+#  group           => '0',
+#  require         => Class['r10k::webhook::config'],
+#  }
+#}
