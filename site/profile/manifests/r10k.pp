4da876134fe5216ad0c6b61bd579cdb9a9bceb1c
Nick Gotsinas <darf@gameforce.ca>
2016-09-15 16:14:25 -0400

    try the webhook with a different manifest

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -3,21 +3,18 @@ class profile::r10k {
 
 class { 'r10k':
   remote => 'git@github.com:gameforce/puppet-core.git',
-}
+  }
 
-# Comment the following out because there is a bug with the webhook
-# Instead of running via mco, run r10k directly
-#class {'r10k::webhook::config':
-#  use_mcollective => false,
-#}
+class { '::r10k::webhook::config':
+  protected        => false,
+  public_key_path  => '/etc/mcollective/server_public.pem',  # Mandatory for FOSS
+  private_key_path => '/etc/mcollective/server_private.pem', # Mandatory for FOSS
+  notify           => Service['webhook'],
+  }
 
-# The hook needs to run as root when not running using mcollective
-# It will issue r10k deploy environment <branch_from_gitlab_payload> -p
-# When git pushes happen.
-#class {'r10k::webhook':
-#  use_mcollective => false,
-#  user            => 'root',
-#  group           => '0',
-#  require         => Class['r10k::webhook::config'],
-#  }
+class { '::r10k::webhook':
+  user    => 'puppet',                                       # Mandatory for FOSS
+  group   => 'puppet',                                       # Mandatory for FOSS
+  require => Class['::r10k::webhook::config'],
+  }
 }
