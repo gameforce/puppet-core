2edebb0df036d77f318e8b7fe51be1d7a4d651aa
Nick Gotsinas <darf@gameforce.ca>
2016-09-18 14:04:42 -0400

    fix typos

--- site/profile/manifests/dnsrecords.pp
+++ site/profile/manifests/dnsrecords.pp
@@ -1,12 +1,12 @@
 class profile::dnsrecords {
 
   resource_record { 'kam.gameforce.net delegation':
-    record        => 'kam.gameforce.net'
+    record        => 'kam.gameforce.net',
     type          => 'NS',
     zone          => 'gameforce.net',
     query_section => 'authority',
     data          => 'kam.gameforce.net.',
-    keyfile => '/etc/named/keys/update',
+    keyfile       => '/etc/named/keys/update',
 }
 
   resource_record { 'kam.gameforce.net servers':
@@ -17,27 +17,28 @@ class profile::dnsrecords {
 }
 
   resource_record { 'gameforce.net mail exchangers':
-    record        => 'gameforce.net'
+    record        => 'gameforce.net',
     type          => 'MX',
     data          => 'mxr.gameforce.net.',
-    keyfile => '/etc/named/keys/update',
+    ttl           => 86400,
+    keyfile       => '/etc/named/keys/update',
 }
 
   resource_record { 'kam.gameforce.net address':
-    ensure  => present,
-    record  => 'kam.gameforce.net',
-    type    => 'A',
-    data    => [ '192.168.11.10' ],
-    ttl     => 86400,
-    keyfile => '/etc/named/keys/update',
+    ensure        => present,
+    record        => 'kam.gameforce.net',
+    type          => 'A',
+    data          => [ '192.168.11.10' ],
+    ttl           => 86400,
+    keyfile       => '/etc/named/keys/update',
   }
 
-  resource_record { 'kam.gameforce.net address':
-    ensure  => present,
-    record  => 'mxr.gameforce.net',
-    type    => 'CNAME',
-    data    => [ 'kam.gameforce.net.' ],
-    ttl     => 86400,
-    keyfile => '/etc/named/keys/update',
+  resource_record { 'mxr.gameforce.net address':
+    ensure        => present,
+    record        => 'mxr.gameforce.net',
+    type          => 'CNAME',
+    data          => [ 'kam.gameforce.net.' ],
+    ttl           => 86400,
+    keyfile       => '/etc/named/keys/update',
   }
 }
