ef44851d82682bec7131e27f9f508c1bfacf9ed8
Nick Gotsinas <darf@gameforce.ca>
2016-09-18 14:01:39 -0400

    add some records

--- site/profile/manifests/dnsrecords.pp
+++ site/profile/manifests/dnsrecords.pp
@@ -1,15 +1,43 @@
 class profile::dnsrecords {
 
+  resource_record { 'kam.gameforce.net delegation':
+    record        => 'kam.gameforce.net'
+    type          => 'NS',
+    zone          => 'gameforce.net',
+    query_section => 'authority',
+    data          => 'kam.gameforce.net.',
+    keyfile => '/etc/named/keys/update',
+}
+
+  resource_record { 'kam.gameforce.net servers':
+    record        => 'kam.gameforce.net'
+    type          => 'NS',
+    data          => 'kam.gameforce.net.',
+    keyfile => '/etc/named/keys/update',
+}
+
+  resource_record { 'gameforce.net mail exchangers':
+    record        => 'gameforce.net'
+    type          => 'MX',
+    data          => 'mxr.gameforce.net.',
+    keyfile => '/etc/named/keys/update',
+}
+
   resource_record { 'kam.gameforce.net address':
     ensure  => present,
     record  => 'kam.gameforce.net',
     type    => 'A',
     data    => [ '192.168.11.10' ],
     ttl     => 86400,
-    zone    => 'gameforce.net',
-    server  => 'kam.gameforce.net',
-    keyname => 'update',
-    keyfile => '/etc/named/keys/local-update'
+    keyfile => '/etc/named/keys/update',
   }
 
+  resource_record { 'kam.gameforce.net address':
+    ensure  => present,
+    record  => 'mxr.gameforce.net',
+    type    => 'CNAME',
+    data    => [ 'kam.gameforce.net.' ],
+    ttl     => 86400,
+    keyfile => '/etc/named/keys/update',
+  }
 }
--- site/profile/manifests/dnszones.pp
+++ site/profile/manifests/dnszones.pp
@@ -1,11 +1,18 @@
 class profile::dnszones {
 
-bind::zone { 'gameforce.net-internal':
-  zone_type       => 'master',
-  domain          => 'gameforce.net',
-  allow_updates   => [ 'key update', ],
-  ns_notify       => true,
-  dnssec          => true,
+  bind::zone { 'gameforce.net-forward':
+    zone_type       => 'master',
+    domain          => 'gameforce.net',
+    allow_updates   => [ 'key update', ],
+    ns_notify       => true,
+    dnssec          => true,
   }
 
+  bind::zone { 'gameforce.net-reverse':
+    zone_type       => 'master',
+    domain          => '11.168.192.in-addr.arpa',
+    allow_updates   => [ 'key update', ],
+    ns_notify       => true,
+    dnssec          => true,
+    }
 }
