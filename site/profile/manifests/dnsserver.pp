1e3694fbd15978b757bb500f45a2bd89226af251
Nick Gotsinas <darf@gameforce.ca>
2016-09-18 14:13:17 -0400

    typos fix

--- site/profile/manifests/dnsserver.pp
+++ site/profile/manifests/dnsserver.pp
@@ -11,8 +11,8 @@ class profile::dnsserver {
   }
 
   bind::key { 'update':
-      algorithm   => hmac-sha256;
-      secret      => "BIPZ6BABG4SA240hijj/2o1L39NYnIt1z8p08N+g2BY=";
+      algorithm   => 'hmac-sha256',
+      secret      => "BIPZ6BABG4SA240hijj/2o1L39NYnIt1z8p08N+g2BY=",
       owner       => 'root',
       group       => 'named',
   }
