8d7b2cfe794f8c09881f66bff0a82abd9d23f62a
Nick Gotsinas <darf@gameforce.ca>
2016-09-18 21:44:54 -0400

    bind configureation

---
+++ site/profile/manifests/bind.pp
@@ -0,0 +1,38 @@
+class profile::bind {
+
+  include bind
+
+  bind::server::conf { '/etc/named.conf':
+    listen_on_addr          => [ 'any' ],
+    listen_on_v6_addr       => [ 'false' ],
+    forwarders              => [ '192.168.11.10', '8.8.8.8', '8.8.4.4' ],
+    allow_query             => [ 'localnets' ],
+    managed_keys_directory  => '/var/named/dynamic',
+    zones                   => {
+      'gameforce.net'       => [
+        'type master',
+        'file "gameforce.net"',
+      ],
+      '11.168.192.in-addr.arpa' => [
+        'type master',
+        'file "11.168.192.in-addr.arpa"',
+      ],
+    },
+
+    keys                 => {
+      'rndc-key' => [
+        'algorithm hmac-md5',
+        'secret "5SRbPvnrL3m72JxYsaYnxw=="',
+      ],
+  }
+
+  bind::server::file { 'gameforce.net':
+  source => 'puppet:///site/bind/gameforce.net',
+  ensure => 'present',
+  }
+
+  bind::server::file { '1.168.192.in-addr.arpa':
+  source => 'puppet:///site/bind/1.168.192.in-addr.arpa',
+  ensure => 'present',
+  }
+}
