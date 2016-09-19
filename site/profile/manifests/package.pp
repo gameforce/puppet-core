d89317abdd57b3d056c7df03616960f46519b0a6
Nick Gotsinas <darf@gameforce.ca>
2016-09-19 18:00:22 -0400

    add bind-tools and wget to default packages

--- site/profile/manifests/package.pp
+++ site/profile/manifests/package.pp
@@ -1,10 +1,12 @@
 # /etc/puppetlabs/code/environments/production/site/profile/manifests/package.pp
 class profile::package {
+  package { 'bind-tools': ensure => 'installed', }
   package { 'epel-release': ensure => 'installed', }
   package { 'htop': ensure => 'installed', }
   package { 'nfs-utils': ensure => 'installed', }
   package { 'nmap': ensure => 'installed', }
   package { 'screen': ensure => 'installed', }
   package { 'vim-enhanced': ensure => 'installed', }
+  package { 'wget': ensure => 'installed', }
   package { 'zsh': ensure => 'installed', }
 }
