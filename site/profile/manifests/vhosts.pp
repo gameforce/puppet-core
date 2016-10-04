5fc5bc94a5429ccf28f2226cb2f7c9d6bbb36487
Nick Gotsinas <darf@gameforce.ca>
2016-10-04 11:55:55 -0400

    ensure the vhosts directory exists

--- site/profile/manifests/vhosts.pp
+++ site/profile/manifests/vhosts.pp
@@ -1,7 +1,16 @@
 class profile::vhosts {
 
+  # Ensure the vhosts directory exists
+  file { '/var/www/html/vhosts':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
   apache::vhost { 'kam':
   port    => '80',
-  docroot => '/var/www/vhosts/kam',
+  docroot => '/var/www/html/vhosts/kam',
   }
+
 }
