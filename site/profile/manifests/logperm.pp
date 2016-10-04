9c427b34e0d417aee7e80b7fa926e4bcc4ae0881
Nick Gotsinas <darf@gameforce.ca>
2016-10-03 21:06:02 -0400

    add logperm class

---
+++ site/profile/manifests/logperm.pp
@@ -0,0 +1,20 @@
+class ::logperm {
+
+  case $::osfamily {
+    'RedHat': {
+      $logfiles = [
+        '/var/log/messages',
+        '/var/log/cron',
+        '/var/log/maillog',
+        '/var/log/spooler',
+        '/var/log/secure'
+      ]
+    }
+    default: {fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}. Please update ::logperm class.")}
+  }
+
+  file { $logfiles:
+    mode => 'a+r',
+  }
+
+}
