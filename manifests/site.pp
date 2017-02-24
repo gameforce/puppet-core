79263fcdaf6663c2b4c61c036699c4b5abeaff65
Nick Gotsinas <darf@gameforce.ca>
2017-02-23 23:54:54 -0800

    remove fuse things

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,2 @@
 # Include hiera classes
 hiera_include('classes')
-
-# Include default modules
-#node default {
-#  include firewall
-#  include ntp
-#  include shell
-#  include user
-#  include package
-#}
