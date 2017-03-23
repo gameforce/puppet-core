32359f224e47ee09dce6cea74ee1d1cef4fa10ea
Nick <nick@gameforce.net>
2017-03-23 14:52:36 -0700

    setup webhook

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -5,17 +5,24 @@ class { 'r10k':
   remote => 'git@github.com:gameforce/puppet-core.git',
   }
 
+ sshkey { "obiwan.gameforce.net":
+  ensure   => present,
+  type     => "ssh-rsa",
+  target   => "/root/.ssh/known_hosts",
+  key      => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWg47Ju199L8HCCnoknYvqjHyT/uWK0w+s8I/U163e3CX6Uux1ud4AvO4tlUZAhdqeCmBzyMjiXWpDhjf1JUrEpgCi9+Si0UO2rNpSJuSadU5acgVIIip5h1Qt8BZX0EIAEWeruM6rnaqstT4NwK9+oGTwdN6ukI8yum37Zh0au62xfH1RJRCcZGYmd6QvGcfDgUizIPa2JztiCQr8UfmEaaC2UreK3TDGgxBT0suRSd9JenpZYIbxePsvLtv/dI9eocskzzlKj+4q7XYKoXtOfGWrJ96yNRMgtCRJC2pESObGT+WtkVYltH6jHzgLH4ClPbRq7vv+iZf2N3E8L/fx r10k"
+ }
+
 class { '::r10k::webhook::config':
   protected        => false,
   use_mcollective => false,
-  public_key_path  => '/etc/mcollective/server_public.pem',  # Mandatory for FOSS
-  private_key_path => '/etc/mcollective/server_private.pem', # Mandatory for FOSS
+  public_key_path  => "/etc/puppetlabs/puppet/ssl/ca/signed/${facts['fqdn']}.pem",
+  private_key_path => "/etc/puppetlabs/puppet/ssl/private_keys/${facts['fqdn']}.pem",
   notify           => Service['webhook'],
   }
 
 class { '::r10k::webhook':
-  user    => 'puppet',                                       # Mandatory for FOSS
-  group   => 'puppet',                                       # Mandatory for FOSS
+  user    => 'puppet',
+  group   => 'puppet',
   require => Class['::r10k::webhook::config'],
   }
 }
