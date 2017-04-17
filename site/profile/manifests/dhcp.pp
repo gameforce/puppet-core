a33422eb28c87ba9bb6189a4e3211f00e5b50836
Nick <nick@gameforce.net>
2017-04-17 11:41:18 -0700

    update dhcp class profile

--- site/profile/manifests/dhcp.pp
+++ site/profile/manifests/dhcp.pp
@@ -1,8 +1,19 @@
 class profile::dhcp {
 
-class { '::dhcpd':
-  configsource  => 'puppet:///files/dhcp/dhcpd.conf',
-# Default is to enable but allow to be stopped (for active/passive)
-  ensure => 'running',
+ class { 'dhcp':
+  service_ensure => running,
+  dnsdomain      => [
+    'gameforce.net',
+    '11.168.192.in-addr.arpa',
+  ],
+  nameservers  => ['192.168.11.10'],
+  ntpservers   => ['us.pool.ntp.org'],
+  interfaces   => ['enp11s0'],
+  dnsupdatekey => '/etc/bind/rndc.key',
+  dnskeyname   => 'rndc-key',
+  require      => Bind::Key['rndc-key'],
+  pxeserver    => '192.168.11.10',
+  pxefilename  => 'pxelinux.0',
+  omapi_port   => 7911,
   }
 }
