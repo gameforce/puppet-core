d661bd4ca7ca897cd0320ee195aa8d0a0095f4c8
Nick <nick@gameforce.net>
2017-08-06 00:05:49 -0700

    change puppetdb port to 8082 to avoid clobbering gitlab

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -1,6 +1,10 @@
 class profile::puppetdb {
   # Configure puppetdb and its underlying database
-  class { 'puppetdb': }
+  
+  class { 'puppetdb': 
+  listen_port => 8082,
+}
+  
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
 }
