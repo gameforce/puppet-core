c1e84c8bf5a54c056af47dd6da3750f2c2be52af
Nick Gotsinas <nick.gotsinas@gmail.com>
2017-08-23 00:14:00 -0700

    remove extra brace

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -2,7 +2,6 @@ class profile::puppetdb {
   
   # Configure puppetdb and its underlying database
   class { 'puppetdb': }
-}
   
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
