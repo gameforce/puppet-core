91878defeb62925e4070a56aae7a8cd4e641ac72
Nick Gotsinas <nick.gotsinas@gmail.com>
2017-08-27 00:31:03 -0700

    enable git package

--- site/profile/manifests/package.pp
+++ site/profile/manifests/package.pp
@@ -1,7 +1,7 @@
 # /etc/puppetlabs/code/environments/production/site/profile/manifests/package.pp
 class profile::package {
   package { 'epel-release': ensure => 'installed', }
-  #package { 'git': ensure => 'installed', }
+  package { 'git': ensure => 'installed', }
   package { 'bind-utils': ensure => 'installed', }
   package { 'nfs-utils': ensure => 'installed', }
   package { 'nmap': ensure => 'installed', }
