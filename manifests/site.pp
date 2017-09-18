48246b1760a409af8ff3488997a94ea3fa0587ed
Nick Gotsinas <nick.gotsinas@gmail.com>
2017-09-17 22:34:49 -0700

    fixed hiera v5 lookup functions

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,7 @@
 # Include hiera classes
-# hiera version 3:
+# Hiera v3:
 # hiera_include('classes')
 
-# hiera version 5:
-lookup('classes')
-#lookup('classes', {merge => unique}).include
-#include lookup('classes', { 'merge' => 'deep' })
-
-#lookup({ 'name'  => 'classes',
-#         'merge' => {
-#            'strategy'          => 'deep',
-#            'merge_hash_arrays' => true,
-#        },
-#})
+# Hiera v5:
+# Merge several arrays of class names into one array:
+lookup('classes', {merge => 'unique'})
