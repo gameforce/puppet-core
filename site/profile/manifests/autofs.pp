62176239d832ff841bf53988a7f78657bbbd4fa8
Nick Gotsinas <nick.gotsinas@gmail.com>
2017-09-17 22:58:01 -0700

    remove use_dir

--- site/profile/manifests/autofs.pp
+++ site/profile/manifests/autofs.pp
@@ -1,11 +1,10 @@
 class profile::autofs {
-
+ include autofs
   autofs::mount { 'net':
     mount       => '/net',
     mapfile     => '/etc/auto.net.data',
     mapcontents => ['* -user,rw,soft,intr,rsize=32768,wsize=32768,tcp,nfsvers=3,noacl sidious:/data-pool'],
     options     => '--timeout=120',
     order       => 01,
-    use_dir     => true
   }
 }
