6947ea361f269ffc305ae70826aeabc2dfe872c5
Nick <nick@gameforce.net>
2017-09-29 22:33:39 -0700

    add common class

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,7 +1,10 @@
-# includes
-include ::openvmtools
+class common {
+  
+  # includes
+  include ::openvmtools
 
-# classes
-class { 'ntp':
-  servers => [ 'clock.gameforce.net', 'sith.gameforce.net' ],
+  # classes
+  class { 'ntp':
+    servers => [ 'clock.gameforce.net', 'sith.gameforce.net' ],
+  }
 }
