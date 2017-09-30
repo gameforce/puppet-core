ca4de8999ccd602ff3a626477628a9b493d11213
Nick Gotsinas <darf@gameforce.ca>
2017-09-29 22:40:39 -0700

    disable the firewall

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,10 +1,14 @@
 class common {
-  
+
   # includes
   include ::openvmtools
 
   # classes
   class { 'ntp':
     servers => [ 'clock.gameforce.net', 'sith.gameforce.net' ],
+  }
+
+  class { 'firewall': 
+    ensure => 'stopped'
   }
 }
