1b73a19901cca0581e8cd95ab833605cebaa8560
Nick <nick@gameforce.net>
2017-09-29 22:31:40 -0700

    disable hiera lookups

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,5 @@
-# Include hiera classes
-# Hiera v3:
-# hiera_include('classes')
-
-# Hiera v5:
 # Merge several arrays of class names into one array:
-lookup('classes', {merge => 'hash'})
+#lookup('classes', {merge => 'hash'})
 
 node default {
   include common
