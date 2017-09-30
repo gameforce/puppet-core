91c0402021eaea1c0e1dce8aa5693bf8bf80f96a
Nick <nick@gameforce.net>
2017-09-29 23:02:32 -0700

    fix syntax error in mount_files

--- manifests/mounts.pp
+++ manifests/mounts.pp
@@ -1,11 +1,11 @@
 class mounts {
   class { 'autofs':
-    'mount_files' => {
-      'home' => {
+    mount_files => {
+      home => {
         mountpoint  => '/home',
         file_source => 'puppet:///files/autofs/auto.home',
       },
-      'net'  => {
+      net  => {
         mountpoint  => '/net',
         file_source => 'puppet:///files/autofs/auto.net',
       }
