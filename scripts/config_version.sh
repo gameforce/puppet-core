2159e047bbbe4a8b4fc35db78a8f3c126fb34b0a
Nick <nick@gameforce.net>
2017-10-07 18:31:58 -0700

    not running in pe

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 if [ -e $1/$2/.r10k-deploy.json ]
 then
-  /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/code_manager_config_version.rb $1 $2
-elif [ -e /opt/puppetlabs/server/pe_version ]
-then
   /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/config_version.rb $1 $2
 else
   /usr/bin/git --version > /dev/null 2>&1 &&
