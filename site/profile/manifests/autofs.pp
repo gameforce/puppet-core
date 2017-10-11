29410bdc3aaa1fd8b6a5830c483df64f5dc65e64
Nick Gotsinas <nick@gameforce.net>
2017-10-10 23:21:43 -0700

    fix file sources

--- site/profile/manifests/autofs.pp
+++ site/profile/manifests/autofs.pp
@@ -6,7 +6,7 @@ file { '/etc/autofs.conf':
   owner  => 'root',
   group  => 'root',
   mode   => '0644',
-  source => 'puppet:///modules/files/autofs/autofs.conf',
+  source => 'puppet:///files/autofs/autofs.conf',
   notify => Service['autofs'],
 }
 
@@ -14,11 +14,11 @@ class { 'autofs':
   mount_files => {
     home => {
       mountpoint  => '/home',
-      file_source => 'puppet:///modules/files/autofs/auto.home',
+      file_source => 'puppet:///files/autofs/auto.home',
       },
     net  => {
     mountpoint  => '/net',
-    file_source => 'puppet:///modules/files/autofs/auto.net',
+    file_source => 'puppet:///files/autofs/auto.net',
       }
     }
   }
