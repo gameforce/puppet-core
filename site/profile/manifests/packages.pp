53aa5453001f697398a98df62906c578ae749e93
Nick <nick@gameforce.net>
2018-05-06 18:42:27 -0700

    do osfamily check for packages

--- site/profile/manifests/packages.pp
+++ site/profile/manifests/packages.pp
@@ -1,12 +1,27 @@
 # /etc/puppetlabs/code/environments/production/site/profile/manifests/package.pp
+
 class profile::packages {
-  package { 'epel-release': ensure => 'installed', }
-  package { 'bind-utils': ensure => 'installed', }
-  package { 'nfs-utils': ensure => 'installed', }
-  package { 'nmap': ensure => 'installed', }
-  package { 'zsh': ensure => 'installed', }
-  package { 'screen': ensure => 'installed', }
-  package { 'vim-enhanced': ensure => 'installed', }
-  package { 'wget': ensure => 'installed', }
-  package { 'htop': ensure => 'installed', }
+
+  if $facts['osfamily'] == 'Debian' {
+    package { 'bind9utils': ensure => 'installed', }
+    package { 'nfs-common': ensure => 'installed', }
+    package { 'nmap': ensure => 'installed', }
+    package { 'zsh': ensure => 'installed', }
+    package { 'screen': ensure => 'installed', }
+    package { 'vim-nox': ensure => 'installed', }
+    package { 'wget': ensure => 'installed', }
+    package { 'htop': ensure => 'installed', }
+
+  elseif $facts['osfamily'] == 'RedHat'  {
+    package { 'epel-release': ensure => 'installed', }
+    package { 'bind-utils': ensure   => 'installed', }
+    package { 'nfs-utils': ensure    => 'installed', }
+    package { 'nmap': ensure         => 'installed', }
+    package { 'zsh': ensure          => 'installed', }
+    package { 'screen': ensure       => 'installed', }
+    package { 'vim-enhanced': ensure => 'installed', }
+    package { 'wget': ensure         => 'installed', }
+    package { 'htop': ensure         => 'installed', }
+    }
+  }
 }
