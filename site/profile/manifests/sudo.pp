86742248acceb82c015769f4d072fd07eca66e15
Nick <nick@gameforce.net>
2018-05-12 12:47:33 -0700

    disable ldap

--- site/profile/manifests/sudo.pp
+++ site/profile/manifests/sudo.pp
@@ -2,6 +2,6 @@ class profile::sudo {
   class { 'sudo': }
     sudo::conf { 'systems':
       source      => 'puppet:///files/sudo/systems.conf',
-      ldap_enable => true,
+      ldap_enable => false,
   }
 }
