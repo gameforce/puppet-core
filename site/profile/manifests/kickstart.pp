88374c6459c7e06aea7b96d1df5478396a98c6c2
Nick <nick@gameforce.net>
2018-05-11 22:26:04 -0700

    fix require to be on one line with an array

--- site/profile/manifests/kickstart.pp
+++ site/profile/manifests/kickstart.pp
@@ -11,8 +11,7 @@ class profile::kickstart {
     group  => 'root',
     mode   => '0644',
     source => 'puppet:///files/pxeboot/default',
-    require => Package['tftp-server'],
-    require => Package['syslinux-tftpboot'],
+    require => Package['tftp-server','syslinux-tftpboot'],
     # notify =>  Service['tftp'],
     # Server Error: Invalid relationship: File[/var/lib/tftpboot/pxelinux.cfg/default] { notify => Service[tftp.service] }, because Service[tftp.service] doesn't seem to be in the catalog
   }
@@ -25,8 +24,7 @@ class profile::kickstart {
     mode   => '0644',
     source => 'puppet:///files/kickstart/centos-desktop.ks',
     notify =>  Service['httpd'],
-    require => Package['tftp-server'],
-    require => Package['syslinux-tftpboot'],
+    require => Package['tftp-server','syslinux-tftpboot'],
   }
 
   file { '/var/www/html/vhosts/repo/ks/centos-server.ks':
@@ -36,7 +34,6 @@ class profile::kickstart {
     mode   => '0644',
     source => 'puppet:///files/kickstart/centos-server.ks',
     notify =>  Service['httpd'],
-    require => Package['tftp-server'],
-    require => Package['syslinux-tftpboot'],
+    require => Package['tftp-server','syslinux-tftpboot'],
   }
 }
