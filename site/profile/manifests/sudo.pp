a04829d6b0ec1044352c3066f767ac621fab70cc
Nick <nick@gameforce.net>
2018-05-12 12:51:02 -0700

    no parameter ldap enable

--- site/profile/manifests/sudo.pp
+++ site/profile/manifests/sudo.pp
@@ -2,6 +2,6 @@ class profile::sudo {
   class { 'sudo': }
     sudo::conf { 'systems':
       source      => 'puppet:///files/sudo/systems.conf',
-      ldap_enable => false,
+      ##ldap_enable => false,
   }
 }
