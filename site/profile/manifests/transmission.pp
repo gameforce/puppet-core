96b1b605e337285317f4806c37cc8ca68e2eed5c
Nick <nick.gotsinas@gmail.com>
2018-07-12 10:46:49 -0700

    getting stuff to work

--- site/profile/manifests/transmission.pp
+++ site/profile/manifests/transmission.pp
@@ -1,5 +1,7 @@
 class { 'transmission':
-  rpc-enabled  => true,
+  transuser    => 'systems',
+  transgroup   => 'systems',
+  rpc-enabled  => 'true',
   rpc-password => '$6$yshB3fNH$gNYCCumlYwENi31r/LYBe4jAqtLsXW1HnlaroUSJtgLK5nUAc8rXu2jdOAbUozuIjmJ2ZKv.N4S4.UwuftrQn/',
   rpc-port     => '9090',
 }
