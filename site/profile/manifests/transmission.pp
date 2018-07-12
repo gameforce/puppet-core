5fe8f476451f769490ffd942619b1dd1d335418d
Nick <nick.gotsinas@gmail.com>
2018-07-12 10:48:56 -0700

    forgot the profile class

--- site/profile/manifests/transmission.pp
+++ site/profile/manifests/transmission.pp
@@ -1,7 +1,9 @@
-class { 'transmission':
-  transuser    => 'systems',
-  transgroup   => 'systems',
-  rpc-enabled  => 'true',
-  rpc-password => '$6$yshB3fNH$gNYCCumlYwENi31r/LYBe4jAqtLsXW1HnlaroUSJtgLK5nUAc8rXu2jdOAbUozuIjmJ2ZKv.N4S4.UwuftrQn/',
-  rpc-port     => '9090',
+class profile::transmission {
+  class { 'transmission':
+    transuser    => 'systems',
+    transgroup   => 'systems',
+    rpc-enabled  => 'true',
+    rpc-password => '$6$yshB3fNH$gNYCCumlYwENi31r/LYBe4jAqtLsXW1HnlaroUSJtgLK5nUAc8rXu2jdOAbUozuIjmJ2ZKv.N4S4.UwuftrQn/',
+    rpc-port     => '9090',
+  }
 }
