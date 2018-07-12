5c2ccc928aab3d55ba33afe739ec4832d45d8b89
Nick <nick.gotsinas@gmail.com>
2018-07-12 10:53:46 -0700

    systems user conflict

--- site/profile/manifests/transmission.pp
+++ site/profile/manifests/transmission.pp
@@ -1,6 +1,6 @@
 class profile::transmission {
   class { 'transmission':
-    transuser    => 'systems',
+  #  transuser    => 'systems',
     transgroup   => 'systems',
     rpc_enabled  => 'true',
     rpc_password => '$6$yshB3fNH$gNYCCumlYwENi31r/LYBe4jAqtLsXW1HnlaroUSJtgLK5nUAc8rXu2jdOAbUozuIjmJ2ZKv.N4S4.UwuftrQn/',
