74a471af92ae5112e5cd3119e182e0b54718a8d2
Nick <nick.gotsinas@gmail.com>
2018-07-12 10:51:53 -0700

    replace dashes with underscores

--- site/profile/manifests/transmission.pp
+++ site/profile/manifests/transmission.pp
@@ -2,8 +2,8 @@ class profile::transmission {
   class { 'transmission':
     transuser    => 'systems',
     transgroup   => 'systems',
-    rpc-enabled  => 'true',
-    rpc-password => '$6$yshB3fNH$gNYCCumlYwENi31r/LYBe4jAqtLsXW1HnlaroUSJtgLK5nUAc8rXu2jdOAbUozuIjmJ2ZKv.N4S4.UwuftrQn/',
-    rpc-port     => '9090',
+    rpc_enabled  => 'true',
+    rpc_password => '$6$yshB3fNH$gNYCCumlYwENi31r/LYBe4jAqtLsXW1HnlaroUSJtgLK5nUAc8rXu2jdOAbUozuIjmJ2ZKv.N4S4.UwuftrQn/',
+    rpc_port     => '9090',
   }
 }
