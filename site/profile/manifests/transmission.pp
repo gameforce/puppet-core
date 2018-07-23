a80d2be064d04b1a6044fc9e55b7b6cc7bd28a82
Nick <nick.gotsinas@gmail.com>
2018-07-22 22:03:53 -0700

    stop using broken transmission class

--- site/profile/manifests/transmission.pp
+++ site/profile/manifests/transmission.pp
@@ -1,11 +1,3 @@
 class profile::transmission {
-    class {'transmission':
-       download_dir   => "/data/systems/downloads",
-       incomplete_dir => "/data/systems/downloads/incomplete",
-       web_port       => 9091,
-       web_whitelist  => ['127.0.0.1'],
-       blocklist_url  => 'http://list.iblocklist.com/?list=bt_level1',
-       web_user       => 'transmission',
-       web_password   => '#thx1138',
-    }
+     #place the files in the right places
 }
