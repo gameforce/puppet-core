78be9363be0c9a36cca93bce707eaed2d3bdf1f3
Nick <nick.gotsinas@gmail.com>
2018-07-23 19:52:10 -0700

    extraneous modules directory

--- site/profile/manifests/plex.pp
+++ site/profile/manifests/plex.pp
@@ -1,13 +1,13 @@
 class profile::plex {
   include 'docker'
 
+  file { '/tmp/docker-compose-plex-host.yml':
+    ensure      => file,
+    source      => 'puppet:///files/docker/docker-compose-plex-host.yml',
+  }
+
   docker_compose { '/tmp/docker-compose-plex-host.yml':
     ensure      => present,
     #subscribe   => File['/tmp/docker-compose-plex-host.yml'],
-  }
-
-  file { '/tmp/docker-compose-plex-host.yml':
-    ensure      => file,
-    source      => 'puppet:///modules/files/docker/docker-compose-plex-host.yml',
   }
 }
