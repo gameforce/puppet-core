ae1864cacb8642684e45c8b818cda69378406b9b
Nick <nick.gotsinas@gmail.com>
2018-07-23 19:46:25 -0700

    missing comma on subscribe

--- site/profile/manifests/plex.pp
+++ site/profile/manifests/plex.pp
@@ -2,13 +2,13 @@ class profile::plex {
   include 'docker'
 
   docker_compose { '/tmp/docker-compose-plex-host.yml':
-    ensure  => present,
-    docker_file => '/tmp/docker-compose-plex-host.yml'
-    subscribe => File['/tmp/docker-compose-plex-host.yml'],
+    ensure      => present,
+    docker_file => '/tmp/docker-compose-plex-host.yml',
+    subscribe   => File['/tmp/docker-compose-plex-host.yml'],
   }
 
   file { '/tmp/docker-compose-plex-host.yml':
-    ensure => file,
-    source => 'puppet:///modules/files/docker/docker-compose-plex-host.yml',
+    ensure      => file,
+    source      => 'puppet:///modules/files/docker/docker-compose-plex-host.yml',
   }
 }
