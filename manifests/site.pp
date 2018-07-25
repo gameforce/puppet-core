56b9baa3ab29ac68b0c72ba3770e842f25a914ed
Nick <nick.gotsinas@gmail.com>
2018-07-25 00:06:09 -0700

    re-factor site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,18 @@
 # Merge several arrays of class names into one array:
 #lookup('classes', {merge => 'hash'})
 #hiera_include('classes')
+
+filebucket { 'main':
+  server => $::servername,
+  path   => false,
+}
+
+File { backup => 'main' }
+
+Package {
+  allow_virtual => true,
+}
+
+node default {
+  ## use of site.pp is deprecated
+}
