0c4fb46182676a1dd8aa3118f41eba698df1ad63
Nick <nick.gotsinas@gmail.com>
2018-07-26 23:41:13 -0700

    log output

--- site/profile/manifests/windows.pp
+++ site/profile/manifests/windows.pp
@@ -8,12 +8,14 @@ class profile::windows {
   exec { 'ExecutionPolicy':
     command   => 'Set-ExecutionPolicy Bypass -Scope Process -Force',
     provider  => powershell,
+    logoutput => true,
  }
 
   # install telnet client
   exec { 'TelnetClient':
     command   => 'Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient"',
     provider  => powershell,
+    logoutput => true,
  }
   # for windows server use windowsfeature
   #windowsfeature { 'TelnetClient':
