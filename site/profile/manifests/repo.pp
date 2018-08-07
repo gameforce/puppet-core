eca64446e484909011805ad3a198f56b12dc7e69
Nick <nick.gotsinas@gmail.com>
2018-08-07 00:04:05 -0700

    too many commas

--- site/profile/manifests/repo.pp
+++ site/profile/manifests/repo.pp
@@ -21,7 +21,7 @@ class profile::repo {
   cron { 'repo_sync':
     command     => '/net/systems/bin/centos_mirror_repo.sh',
     hour        => '2-4',
-    monthday    => '1',,
+    monthday    => '1',
     user        => 'root',
   }
 }
