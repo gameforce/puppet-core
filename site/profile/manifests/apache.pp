1778ceead8f2542ac146398f12034a00961ee656
Nick <nick.gotsinas@gmail.com>
2018-08-07 16:36:58 -0700

    add vhost for vip

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -5,6 +5,12 @@ class profile::apache {
   default_vhost => false,
   }
 
+  # vhost definition for vip
+  apache::vhost { 'vip.gameforce.net':
+  port    => '80',
+  docroot => '/var/www/html/vhosts/vip',
+  }
+
   # enable apache modules
   class { 'apache::mod::ssl': }
   class { 'apache::mod::wsgi': }
