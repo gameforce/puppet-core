4552494771b6609fd6e3db7b98f3a54d5379ed95
Nick <nick.gotsinas@gmail.com>
2018-08-09 20:32:32 -0700

    rename vhost to kam

---
+++ site/profile/manifests/apache/vhosts/kam.pp
@@ -0,0 +1,8 @@
+class profile::apache::vhosts::kam {
+
+  # vhost definitions: kickstart
+  apache::vhost { 'kam':
+  port    => '80',
+  docroot => '/var/www/html/vhosts/kam',
+  }
+}
--- site/profile/manifests/apache/vhosts/kickstart.pp
+++
@@ -1,8 +0,0 @@
-class profile::apache::vhosts::kickstart {
-
-  # vhost definitions: kickstart
-  apache::vhost { 'kickstart':
-  port    => '80',
-  docroot => '/var/www/html/vhosts/kickstart',
-  }
-}
