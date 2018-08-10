bc483c46498f77dc7e4ef2e5c71ee563bc3e90e0
Nick <nick.gotsinas@gmail.com>
2018-08-09 20:31:51 -0700

    add boot folder to web root

--- site/profile/manifests/boot.pp
+++ site/profile/manifests/boot.pp
@@ -6,38 +6,32 @@ class profile::boot {
 
   file { '/var/lib/tftpboot/pxelinux.cfg':
     ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
+
 # pxe default menu
   file { '/var/lib/tftpboot/pxelinux.cfg/default':
     ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/profile/files/boot/default',
     require => File['/var/lib/tftpboot/pxelinux.cfg'],
     # notify =>  Service['tftp'],
     # Server Error: Invalid relationship: File[/var/lib/tftpboot/pxelinux.cfg/default] { notify => Service[tftp.service] }, because Service[tftp.service] doesn't seem to be in the catalog
   }
 
+# boot directory for unattended installs
+  file { '/var/www/html/vhosts/kam/boot':
+    ensure => 'directory',
+  }
+
 # ignition
-  file { '/var/www/html/vhosts/kam/ignition.json':
+  file { '/var/www/html/vhosts/kam/boot/ignition.json':
     ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/profile/files/boot/ignition.json',
     notify =>  Service['httpd'],
   }
 
   # kickstart
-  file { '/var/www/html/vhosts/kam/centos.ks':
+  file { '/var/www/html/vhosts/kam/boot/centos.ks':
     ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/profile/files/boot/centos.ks',
     notify =>  Service['httpd'],
   }
