fc7b39973955c308d7987ca8405f059b9afad51e
Nick <nick.gotsinas@gmail.com>
2018-08-10 22:31:26 -0700

    follow symlinks and allow directory browsing

--- site/profile/manifests/apache/vhosts/kam.pp
+++ site/profile/manifests/apache/vhosts/kam.pp
@@ -4,6 +4,6 @@ class profile::apache::vhosts::kam {
   apache::vhost { 'kam':
   port    => '80',
   docroot => '/var/www/html/vhosts/kam',
-  options => ['+FollowSymLinks', '-Indexes']
+  options => ['+FollowSymLinks', '+Indexes', 'MultiViews']
   }
 }
