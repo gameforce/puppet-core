aaefdadbb2c8d1a5838fe3c1e1bc54102d4c29ea
Nick <nick.gotsinas@gmail.com>
2018-08-27 22:36:15 -0700

    remove puppetdb

--- site/role/manifests/kam.pp
+++ site/role/manifests/kam.pp
@@ -1,10 +1,10 @@
 class role::kam {
   #All roles should include the common profile
   include profile::common
-  include profile::puppet::puppetdb
+  #include profile::puppet::puppetdb
   include profile::puppet::r10k
   include profile::puppet::webhook
-  include profile::puppet::puppetboard
+  #include profile::puppet::puppetboard
   include profile::bind
   include profile::dhcp
   include profile::boot
