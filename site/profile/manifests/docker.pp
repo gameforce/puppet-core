305313b0e3653efc17a60a55d062cee49ca4b2a9
Nick <nick.gotsinas@gmail.com>
2018-10-08 02:39:46 -0700

    fix spacing

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -1,8 +1,9 @@
 class profile::docker {
   include 'docker'
 
+  # ensure docker-compose is present
   class {'docker::compose':
-  ensure => present,
-  version => '1.9.0',
+    ensure => present,
+    version => '1.9.0',
   }
 }
