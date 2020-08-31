064fa7ac3e4129b70ad1cd128972107ab2d28add
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-08-31 01:41:42 -0400

    add version

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -1,6 +1,7 @@
 class profile::docker {
   class { 'docker':
-  use_upstream_package_source => false,
+    use_upstream_package_source => false,
+    version => '17.09.0~ce-0~debian',
 }
 
   # ensure docker-compose is present
