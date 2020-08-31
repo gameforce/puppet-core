64bfb98562fce9db4b87ec0160332907a774e5fa
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-08-31 02:02:24 -0400

    darf already declared

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -2,7 +2,6 @@ class profile::docker {
   class { 'docker':
     use_upstream_package_source => false,
     version                     => 'latest',
-    docker_users                => ['darf'],
 }
 
   # ensure docker-compose is present
