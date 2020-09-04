119929417fe5611be2c36e2a2b65abfb067521f0
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-04 04:29:11 -0400

    no puppetboard class yet

--- site/role/manifests/puppet.pp
+++ site/role/manifests/puppet.pp
@@ -4,7 +4,7 @@ class role::puppet {
   include profile::puppet::puppetdb
   include profile::puppet::r10k
   include profile::puppet::webhook
-  include profile::puppet::puppetboard
+  #include profile::puppet::puppetboard
   ##include profile::bind
   ##include profile::dhcp
 }
