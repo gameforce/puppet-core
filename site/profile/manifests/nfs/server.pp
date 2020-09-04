8b0d7e278e510efb8e71193db4a49ec857def4af
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-04 04:05:08 -0400

    fix nfs server shares

--- site/profile/manifests/nfs/server.pp
+++ site/profile/manifests/nfs/server.pp
@@ -4,8 +4,14 @@ class profile::nfs::server {
   }
 
   # exports for nfs
-  nfs::server::export{ '/data':
+  nfs::server::export{ '/share/data':
     ensure  => 'mounted',
-    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash) localhost(rw)'
+    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
+  nfs::server::export{ '/share/home':
+    ensure  => 'mounted',
+    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
+  nfs::server::export{ '/share/media':
+    ensure  => 'mounted',
+    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
   }
 }
