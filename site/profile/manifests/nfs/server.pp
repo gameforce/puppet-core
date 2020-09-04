b1bb1f04b486bd384a43dbbf35f108a28bebf7cf
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-04 04:07:44 -0400

    missing braces

--- site/profile/manifests/nfs/server.pp
+++ site/profile/manifests/nfs/server.pp
@@ -7,9 +7,11 @@ class profile::nfs::server {
   nfs::server::export{ '/share/data':
     ensure  => 'mounted',
     clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
+  }
   nfs::server::export{ '/share/home':
     ensure  => 'mounted',
     clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
+  }
   nfs::server::export{ '/share/media':
     ensure  => 'mounted',
     clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
