570da58e246a06daf45e6d66b632d842c83bb742
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-04 04:10:37 -0400

    unknown word nolock

--- site/profile/manifests/nfs/server.pp
+++ site/profile/manifests/nfs/server.pp
@@ -6,14 +6,14 @@ class profile::nfs::server {
   # exports for nfs
   nfs::server::export{ '/share/data':
     ensure  => 'mounted',
-    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
+    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash) localhost(rw)',
   }
   nfs::server::export{ '/share/home':
     ensure  => 'mounted',
-    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
+    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash) localhost(rw)',
   }
   nfs::server::export{ '/share/media':
     ensure  => 'mounted',
-    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash,nolock) localhost(rw)',
+    clients => '192.168.11.0/24(rw,insecure,async,no_root_squash) localhost(rw)',
   }
 }
