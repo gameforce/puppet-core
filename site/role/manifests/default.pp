555ef3afb299238527e6f309d97c7e94cf97db26
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-08 11:10:25 -0400

    fix typos

--- site/role/manifests/default.pp
+++ site/role/manifests/default.pp
@@ -1,11 +1,10 @@
 class role::default {
-  # Common classes to all nodes
-  include profile::sudo
-  include profile::packages
-  include profile::cron
   #All roles should include the common profile
   include profile::common
   include profile::autofs
   include profile::ssh
   include profile::users
+  include profile::sudo
+  include profile::packages
+  include profile::cron
 }
