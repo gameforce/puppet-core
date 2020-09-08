72971ee32b779a92167860a6425d27cbe2768a1d
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-08 10:15:55 -0400

    webhook support

--- site/role/manifests/puppet.pp
+++ site/role/manifests/puppet.pp
@@ -3,7 +3,7 @@ class role::puppet {
   include profile::common
   include profile::puppet::puppetdb
   include profile::puppet::r10k
-  #include profile::puppet::webhook
+  include profile::puppet::webhook
   #include profile::puppet::puppetboard
   ##include profile::bind
   ##include profile::dhcp
