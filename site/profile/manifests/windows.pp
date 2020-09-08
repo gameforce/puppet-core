565a039500ab1d4bd39306c47eb4d48947e0a49b
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-08 01:41:44 -0400

    oops

--- site/profile/manifests/windows.pp
+++ site/profile/manifests/windows.pp
@@ -6,8 +6,7 @@ class profile::windows {
     unless   => 'if ((Get-ExecutionPolicy -Scope LocalMachine).ToString() -eq "Unrestricted") { exit 0 } else { exit 1 }',
     provider => powershell,
   }
-
-   install telnet client TODO: install puppetlabs-dism for this https://github.com/puppetlabs/puppetlabs-dism
+  # install telnet client TODO: install puppetlabs-dism for this https://github.com/puppetlabs/puppetlabs-dism
   exec { 'TelnetClient':
     command  => 'Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient"',
     unless   => "if (Get-WindowsOptionalFeature -Online -FeatureName TelnetClient) { exit 1 } else { exit 0 }",
