5a23e9bcf4eaf46f90aa546315c84e1c988eb42a
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-08 00:24:57 -0400

    re-enable ssl for puppetdb

--- site/profile/manifests/puppet/puppetdb.pp
+++ site/profile/manifests/puppet/puppetdb.pp
@@ -3,8 +3,6 @@ class  profile::puppet::puppetdb {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
     listen_address =>  '0.0.0.0',
-    disable_ssl    =>  true,
-    #ssl_listen_port => '8081',
   }
 
   # Configure the Puppet master to use puppetdb
