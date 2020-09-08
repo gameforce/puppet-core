6302eb82f2fbb838217599f7839efbb2a6358923
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-08 01:22:41 -0400

    where the fuck is sudo coming from

--- site/profile/manifests/windows.pp
+++ site/profile/manifests/windows.pp
@@ -8,11 +8,11 @@ class profile::windows {
   }
 
   # install telnet client TODO: install puppetlabs-dism for this https://github.com/puppetlabs/puppetlabs-dism
-  exec { 'TelnetClient':
-    command  => 'Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient"',
-    unless   => "if (Get-WindowsOptionalFeature -Online -FeatureName TelnetClient) { exit 1 } else { exit 0 }",
-    provider => powershell,
-}
+  #exec { 'TelnetClient':
+  #  command  => 'Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient"',
+  #  unless   => "if (Get-WindowsOptionalFeature -Online -FeatureName TelnetClient) { exit 1 } else { exit 0 }",
+  #  provider => powershell,
+#}
   # for windows server use windowsfeature
   #windowsfeature { 'TelnetClient':
   #ensure => present,
