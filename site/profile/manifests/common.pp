c08f8edb233448dcf050dcf9cf11bc6392343b9d
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-08 01:31:27 -0400

    move ssh config to its own profile

--- site/profile/manifests/common.pp
+++ site/profile/manifests/common.pp
@@ -38,26 +38,4 @@ class profile::common {
     source   => 'https://github.com/gameforce/dotfiles.git',
     revision => 'master',
   }
-
-  # sshd config
-  include profile::ssh::server
-  include profile::ssh::client
-
-  exec {'shosts.equiv':
-    command => '/bin/cat /etc/ssh/ssh_known_hosts | /bin/grep -v "^#" | /bin/awk \'{print $1}\' | /bin/sed -e \'s/,/\n/g\' > /etc/ssh/shosts.equiv',
-    require => Class['ssh::knownhosts'],
-  }
-
-    # site specific environment
-    $sitepath = '/net/systems/bin:/opt/puppetlabs/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin'
-    file { '/etc/profile.d/site.sh':
-      content => "export PATH=${sitepath}\n",
-      mode    => '0644'
-    }
-
-    # set locale
-    class { 'locales':
-        default_locale => 'en_US.UTF-8',
-        locales        => ['en_US.UTF-8 UTF-8'],
-    }
 }
--- site/role/manifests/default.pp
+++ site/role/manifests/default.pp
@@ -6,4 +6,5 @@ class role::default {
   #All roles should include the common profile
   include profile::common
   include profile::autofs
+  include profile::ssh
 }
