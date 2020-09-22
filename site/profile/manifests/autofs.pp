5d3efa325bd053ff7a1e5f74cb3bf007d18dd44d
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-22 12:24:59 -0400

    move autofs inside

--- site/profile/manifests/autofs.pp
+++ site/profile/manifests/autofs.pp
@@ -11,4 +11,20 @@ file { '/etc/autofs.conf':
   source => 'puppet:///modules/profile/autofs/autofs.conf',
   notify => Service['autofs'],
   }
+
+# mounts
+autofs::mount { 'nas':
+  mount       => '/net',
+  mapfile     => '/etc/auto.nas',
+  options     => '--timeout=120'
+  }
+
+autofs::mapfile { 'nas':
+  path     => '/etc/auto.nas',
+  mappings => [
+    { 'key' => 'data', 'options' => 'rw,soft,intr', 'fs' => '192.168.11.10:/share/data' },
+    { 'key' => 'media', 'options' => 'rw,soft,intr', 'fs' => '192.168.11.10:/share/media' },
+    { 'key' => 'home', 'options' => 'rw,soft,intr', 'fs' => '192.168.11.10:/share/home' },
+  ]
+  }
 }
