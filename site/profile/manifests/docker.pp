ea69bcd025899d3467b8e356d4a8eb0da589b2f2
Nick Gotsinas <nick.gotsinas@gmail.com>
2020-09-22 12:37:00 -0400

    remove version from docker

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -1,7 +1,7 @@
 class profile::docker {
   class { 'docker':
     use_upstream_package_source => false,
-    version                     => '5:19.03.12~3-0~ubuntu-focal',
+    #version                     => '5:19.03.12~3-0~ubuntu-focal',
 }
 
   # ensure docker-compose is present
