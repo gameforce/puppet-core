a4fdf9221bf583df5b0c8706614f6e848ab946e3
darf <darf@gameforce.net>
2021-05-09 13:59:08 -0400

    set manag_firewall to false in puppetdb

--- site/profile/manifests/puppet/puppetdb.pp
+++ site/profile/manifests/puppet/puppetdb.pp
@@ -3,6 +3,7 @@ class  profile::puppet::puppetdb {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
     listen_address =>  '0.0.0.0',
+    manage_firewall => 'false'
   }
 
   # Configure the Puppet master to use puppetdb
