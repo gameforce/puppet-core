edd461d04db4a6c17d3b8938b94789a50bb4af9d
darf <darf@gameforce.net>
2021-05-09 14:59:05 -0400

    set mnage_firewall at hiera

--- site/profile/manifests/puppet/puppetdb.pp
+++ site/profile/manifests/puppet/puppetdb.pp
@@ -3,7 +3,6 @@ class  profile::puppet::puppetdb {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
     listen_address  => '0.0.0.0',
-    manage_firewall => 'true'
   }
 
   # Configure the Puppet master to use puppetdb
