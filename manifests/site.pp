f4052ff523e37f4adccee880a988c84847f61af9
darf <darf@gameforce.net>
2021-05-09 13:43:11 -0400

    fixing and updating various bits - lookups

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,5 @@
 # Merge several arrays of class names into one array:
-lookup('classes', {merge => 'hash'})
+#lookup('classes', {merge => 'hash'})
 hiera_include('classes')
 #lookup('classes', {merge => unique}).include
 
