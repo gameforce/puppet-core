bc2706e054f39e49ae4be4ef9e3be6ae6c42313b
darf <darf@gameforce.net>
2021-05-09 14:44:56 -0400

    fixing locales again

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -21,6 +21,6 @@ class profile::base {
     # set locale
     class { 'locales':
       default_locale   => 'en_US.UTF-8',
-      locales          => ['en_US.UTF-8','en_CA.UTF-8','UTF-8']
+      locales          => ['en_US.UTF-8 UTF-8']
   }
 }
