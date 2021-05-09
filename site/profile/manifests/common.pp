ab8e7157f2815c11f17fa74f10f6d62a5c94c0ca
darf <darf@gameforce.net>
2021-05-08 21:03:24 -0400

    remove locale because it breaks ubuntu

--- site/profile/manifests/common.pp
+++ site/profile/manifests/common.pp
@@ -68,8 +68,8 @@ class profile::common {
     }
 
     # set locale
-    class { 'locales':
-        default_locale => 'en_US.UTF-8',
-        locales        => ['en_US.UTF-8 UTF-8'],
-    }
+    #class { 'locales':
+    #    default_locale => 'en_US.UTF-8',
+    #    locales        => ['en_US.UTF-8 UTF-8'],
+    #}
 }
